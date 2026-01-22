90e44293c0becb0da67ae67fbaac9754f8bd87e4
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-01-22 21:33:43 +0600

    problem1

---
+++ problem1.js
@@ -0,0 +1,13 @@
+// Supermarket shopping math problem
+
+
+var myTotal = 600;
+var applePrice = 300;
+var orangePrice = 180;
+
+
+var totalCost = applePrice + orangePrice;
+
+var returnMoney = myTotal - totalCost;
+
+console.log(returnMoney);
