a45ed76dbcbb39d5627bbaefa2cc3e4087721a2a
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-01-22 21:57:30 +0600

    problem4

---
+++ problem4.js
@@ -0,0 +1,10 @@
+// Boolean
+
+const a = isNaN("11"); // false
+console.log(a)
+
+const b = isNaN(2-10); // false
+console.log(b);
+
+
+//both of the statements are false cz '11' and 2-10= -8 are numbers. but isNaN means 'is not a number' 
