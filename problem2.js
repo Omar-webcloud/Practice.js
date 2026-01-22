a465465553f24114c4fb10757a23516ce22242f8
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-01-22 21:49:16 +0600

    prblm 2

---
+++ problem2.js
@@ -0,0 +1,12 @@
+// Marks avg output
+
+var Math = 72.5;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+
+var totalMarks = Math + Biology + Chemistry + Physics + Bangla;
+var avgMarks = totalMarks/5;
+
+console.log(avgMarks)
