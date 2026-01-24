b3c2e4bee666a0d35897a5bb7cb72409e7b7d3f7
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-01-24 21:01:03 +0600

    prctc2

---
+++ tour.js
@@ -0,0 +1,8 @@
+const trips = ["Cox Bazar", "Bandarban", "Rangamati"];
+trips.push("Patenga");
+
+trips.push("Sitakunda", "Patuakhali");
+
+trips.pop();
+
+console.log(trips) // [ 'Cox Bazar', 'Bandarban', 'Rangamati', 'Patenga', 'Sitakunda' ]
