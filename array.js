312bc56cb6427f4ce880aba3c891fb892d459043
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-01-24 20:56:18 +0600

    prctc1

---
+++ array.js
@@ -0,0 +1,6 @@
+const fruits = ["mango", "apple", "banana", "papaya", "melon"];
+console.log(fruits[2])  // banana
+
+
+fruits[1] = "jambura";
+console.log(fruits) // [ 'mango', 'jambura', 'banana', 'papaya', 'melon' ]
