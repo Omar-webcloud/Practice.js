79b06d5cba8a8342e987b1bc4c0d21869730126e
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-01-27 22:12:15 +0600

    str 3

--- string.js
+++ string.js
@@ -1,7 +1,16 @@
-// 1
+// 1 | 2
 let name = "Abbas";
 
 count = name.toLowerCase().split("a").length - 1;
 console.log(count);
 
 
+// 3 
+
+let sentence = "You are Bangladeshi";
+
+if(sentence.includes("a" && "e" && "i" && "o" && "u" )){
+console.log("vowel shob ase")
+}else{
+ console.log("vowel missing")
+}
